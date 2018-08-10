f149dcc2f025a5383dc50bb93e6fc36a9838eeb5
blue18 <garcb969@icloud.com>
2018-08-10 03:44:38 -0700

    fixing issues

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // August 9, 2018
-
-const url = require('url');
+const express = require('express');
+var app = express();
+const bodyParse = require('body-parser');
 const http = require('http');
 const filesystem = require('fs');
 
@@ -17,10 +18,23 @@ const server = http.createServer((request, response) => {
             response.write(fileContent);
             response.end();
         }
-
     });
-
 });
 
 // Telling the server to listen on port 3000
 server.listen(3000);
+
+
+app.use(bodyParse.urlencoded({ extended: true }));
+
+// Message is received in this function
+app.post('/action', (request, response) => {
+    response.send('You send the name "' + request.body.email + '".');
+    console.log(request.body.email);
+});
+
+app.listen(3001, function() {
+    console.log("Server running at http://127.0.0.1:3001/")
+})
+
+
