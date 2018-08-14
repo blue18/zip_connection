b8f98ff2b785e2f1a01fb1a3ce8cf92131b80ffd
blue18 <garcb969@icloud.com>
2018-08-14 13:48:31 -0700

    created sessions

--- api/login.js
+++ api/login.js
@@ -3,27 +3,11 @@
 const router = require('express').Router();
 
 router.get('/', function(request, response) {
-    // render the page and pass in any flash data if it exists
+    var name = request.session.name;
+    console.log("Name: " + name);
     response.render('login.ejs');
-});
 
-// Message is received in this function
-router.post('/', function (request, response) {
-    try {
-        var email = request.body.email;
-        var password = request.body.password;
-
-        response.send('You send the email "' + email + '". Password: ' + password );
-
-        if(email == "test@gmail.com" && password == "usa123") {
-            console.log("Email and password matched...");
-        } else {
-            console.log("Email or password failed...");
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
+    // render the page and pass in any flash data if it exists
 });
 
 exports.router = router;
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var flash = require('connect-flash');
 
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 // Set up the template engine
 app.set('view engine', 'ejs');
@@ -18,7 +19,13 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev')); // log every request to the console
-
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(session(
+    { 
+        secret: 'this-is-a-token', 
+        resave: false, 
+        saveUninitialized: true 
+}));
 
 // All routes for the API are written in modules in the api/ directory.  The
 // top-level router lives in api/index.js.  That's what we include here, and
@@ -27,10 +34,13 @@ app.use('/', api);
 
 // Sending the index page
 app.get('/', function (request, response) {
+    var sessionData = request.session;
+    sessionData.name = "joe";
+    console.log("You have visited this page: " + sessionData.count);
     response.render('index');
 });
 
 // Listen at localhost:3000 
 app.listen(port, function() {    
-    console.log("Example app listening at http://localhost:" + port);
+    console.log("Listening at http://localhost:" + port);
 });
